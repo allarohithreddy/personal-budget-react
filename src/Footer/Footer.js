9b1fbb4bfedfe0897ece6f5ba9e39299a96401aa
Rohith Reddy Alla <ralla2@uncc.edu>
2024-09-25 13:10:11 -0400

    initial commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom" role="contentinfo">
+        <div className="center">
+            All rights reserved &copy; Rohith Reddy Alla
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Hero() {
+  return (
+    <header className="hero" aria-label="banner">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+    </header>
+  );
+}
+
+export default Hero;
